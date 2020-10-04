2a4d3c6307b2eca6227a86044b127cfdc6316128
Leonidas Yopan <leonidasyopan@gmail.com>
2020-10-03 22:22:35 -0300

    First commit

---
+++ lib/login.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static const routeName = 'LOGIN_PAGE';
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController(text: 'contato@manacespereira.com.br');
+  final _passwordController = TextEditingController(text: '123123');
+
+  @override
+  Widget build(BuildContext context) {
+    void _authenticate() {}
+
+    return Scaffold(
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: _emailController,
+              ),
+              SizedBox(height: 30),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      onPressed: _authenticate,
+                      child: Text('Acessar HomePage'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:bloc_youtube/login.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      routes: <String, WidgetBuilder>{
+        MyHomePage.routeName: (context) => MyHomePage(title: 'Flutter Demo Home Page'),
+        LoginPage.routeName: (context) => LoginPage(),
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  static const routeName = 'HOME_PAGE';
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Something',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
